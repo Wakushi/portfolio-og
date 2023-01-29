8d0499ade83bc83957b5a58e2c039abf04e5fcb6
Wakushi <mzozord@gmail.com>
2023-01-29 17:35:13 +0100

    added Floe to projects

--- src/projectData.js
+++ src/projectData.js
@@ -4,15 +4,16 @@ import meme from "./assets/meme.png"
 import airbnb from "./assets/airbnb.png"
 import tenzies from "./assets/tenzies.png"
 import weabove from "./assets/weabove.png"
+import floe from "./assets/floe.png"
 
 export default [
     {
-        img:notes,
-        desc:"This is a classic notes app that I made using React. I was able to learn this through a Scrimba React course. It was a lot of fun to do !",
-        type:"Web App",
-        title:"Notes App",
-        gitlink:"https://github.com/Wakushi/notes-app",
-        link:"none"
+        img:weabove,
+        desc:"This is an app that calls to a blockchain API and calculates staking rewards for the Web3 project 'WeAbove'",
+        type:"Web3 App",
+        title:"$GRV Calculator",
+        gitlink:"https://github.com/Wakushi/weabove-gravite-calculator",
+        link:"https://weabove-gravite-calculator.vercel.app/"
     },
     {
         img:tenzies,
@@ -23,12 +24,36 @@ export default [
         link:"https://tenzies-henna.vercel.app/"
     },
     {
-        img:weabove,
-        desc:"This is an app that calls to a blockchain API and calculates staking rewards for the Web3 project 'WeAbove'",
-        type:"Web3 App",
-        title:"$GRV Calculator",
-        gitlink:"https://github.com/Wakushi/weabove-gravite-calculator",
-        link:"https://weabove-gravite-calculator.vercel.app/"
+        img:meme,
+        desc:"A classic meme generator app using API calls within React. ",
+        type:"Web App",
+        title:"Meme generator",
+        gitlink:"https://github.com/Wakushi/meme-generator",
+        link:"https://meme-generator-wheat-xi.vercel.app/"
+    },
+    {
+        img:floe,
+        desc:"I learned to do a twitter clone using HTML and JS, and therefore I made the project from scratch using React to train myself",
+        type:"Web App",
+        title:"Twitter clone",
+        gitlink:"https://github.com/Wakushi/floe",
+        link:"https://floe-eta.vercel.app/"
+    },
+    {
+        img:airbnb,
+        desc:"I learned to do a clone of the AirBnb Experiences, and therefore how to correctly iterate over data to create experiences cards components. ",
+        type:"Web App",
+        title:"AirBnb clone",
+        gitlink:"none",
+        link:"none"
+    },
+    {
+        img:notes,
+        desc:"This is a classic notes app that I made using React. I was able to learn this through a Scrimba React course. It was a lot of fun to do !",
+        type:"Web App",
+        title:"Notes App",
+        gitlink:"https://github.com/Wakushi/notes-app",
+        link:"none"
     },
     {
         img:travel,
@@ -38,20 +63,5 @@ export default [
         gitlink:"none",
         link:"https://remarkable-bienenstitch-ae4c5b.netlify.app/"
     },
-    {
-        img:meme,
-        desc:"A classic meme generator app using API calls within React. ",
-        type:"Web App",
-        title:"Meme generator",
-        gitlink:"https://github.com/Wakushi/meme-generator",
-        link:"https://meme-generator-wheat-xi.vercel.app/"
-    },
-    {
-        img:airbnb,
-        desc:"I learned to do a clone of the AirBnb Experiences, and therefore how to correctly iterate over data to create experiences cards components. ",
-        type:"Web App",
-        title:"AirBnb clone",
-        gitlink:"none",
-        link:"none"
-    }
+    
 ]
