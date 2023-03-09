6bea18af167ab2d5c31ee2ae8561136f2eb59610
Wakushi <mzozord@gmail.com>
2023-03-09 12:06:23 +0100

    added wip projects + fixed css

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -9,7 +9,7 @@ export default function Presentation(){
             <div className="presentation-text">
                 <h1>Hi ! I’m Maxime. <span>A Web Developer.</span></h1>
                 <p>I’m a student in web development spending every day learning and experimenting. I have a creative mind and I'm always passionate for the projects I'm involved in.</p>
-                <h4 className="learning">I'm currently learning : <span>React</span></h4>
+                <h4 className="learning">I'm currently learning : <span>Solidity</span></h4>
             </div>
             
             <div className="presentation-animation">
--- src/wipData.js
+++ src/wipData.js
@@ -1,13 +1,30 @@
-import doors from "./assets/doors.png"
-import quizz from "./assets/quizz.png"
+import doors from "./assets/doors.png";
+import waDashboard from "./assets/wa-dashboard.png";
+import kanga from "./assets/kanga.png";
 
 export default [
-    {
-        img:doors,
-        desc:"Doors is a Reigns-like game. You have to take the right decision, but this time to escape a weird and nosy narrator.",
-        type:"Web Game",
-        title:"Doors",
-        gitlink:"https://github.com/Wakushi/doors-v2",
-        link:"https://doors-v2.vercel.app/"
-    },
-]
+  {
+    img: kanga,
+    desc: "Kanga is an image generative website based on AI, which goal is to make text-to-image more user-friendly with different features.",
+    type: "Web App",
+    title: "Kanga",
+    gitlink: "none",
+    link: "none",
+  },
+  {
+    img: waDashboard,
+    desc: "WeAbove Community acts as a dashboard for WeAbove holders to check and optimize their rewards on multiple wallets.",
+    type: "Web3 App",
+    title: "WeAbove Community",
+    gitlink: "none",
+    link: "none",
+  },
+  {
+    img: doors,
+    desc: "Doors is a Reigns-like game. You have to take the right decision, but this time to escape a weird and nosy narrator.",
+    type: "Web Game",
+    title: "Doors",
+    gitlink: "https://github.com/Wakushi/doors-v2",
+    link: "https://doors-v2.vercel.app/",
+  },
+];
