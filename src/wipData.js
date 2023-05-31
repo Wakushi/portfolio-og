d657686193929976571c1c8d0850a1897da6ac52
Wakushi <mzozord@gmail.com>
2023-05-31 22:53:18 +0200

    - Removed doors - Added paths

--- src/wipData.js
+++ src/wipData.js
@@ -1,31 +1,30 @@
-import doors from "./assets/images/doors.png";
-import waDashboard from "./assets/images/wa-dashboard.png";
-import quizAngular from "./assets/images/quiz-angular.png";
+import paths from "./assets/images/paths.png"
+import waDashboard from "./assets/images/wa-dashboard.png"
+import quizAngular from "./assets/images/quiz-angular.png"
 
 export default [
- 
-  {
-    img: waDashboard,
-    desc: "WeAbove Community acts as a dashboard for WeAbove holders to check and optimize their rewards on multiple wallets.",
-    type: "Web3 App",
-    title: "WeAbove Community",
-    gitlink: "none",
-    link: "none",
-  },
-  {
-    img: quizAngular,
-    desc: "QuizMaster is a dynamic and interactive quiz application built using Angular and Firebase.",
-    type: "Web Game",
-    title: "Quiz Master",
-    gitlink: "https://github.com/Wakushi/quiz-app-angular",
-    link: "https://quiz-master-angular.vercel.app/",
-  },
-  {
-    img: doors,
-    desc: "Doors is a Reigns-like game. You have to take the right decision, but this time to escape a weird and nosy narrator.",
-    type: "Web Game",
-    title: "Doors",
-    gitlink: "https://github.com/Wakushi/doors-v2",
-    link: "https://doors-v2.vercel.app/",
-  },
-];
+	{
+		img: waDashboard,
+		desc: "WeAbove Community acts as a dashboard for WeAbove holders to check and optimize their rewards on multiple wallets.",
+		type: "Web3 App",
+		title: "WeAbove Community",
+		gitlink: "none",
+		link: "none"
+	},
+	{
+		img: quizAngular,
+		desc: "QuizMaster is a dynamic and interactive quiz application built using Angular and Firebase.",
+		type: "Web Game",
+		title: "Quiz Master",
+		gitlink: "https://github.com/Wakushi/quiz-app-angular",
+		link: "https://quiz-master-angular.vercel.app/"
+	},
+	{
+		img: paths,
+		desc: "Paths is a Reigns-like game. You're a time and space traveler, captain of your own ship. All of your decisions will impact the fate of your crew and your journey through the universe.",
+		type: "Mobile Game",
+		title: "Paths",
+		gitlink: "https://github.com/Wakushi/paths",
+		link: "https://paths-one.vercel.app/"
+	}
+]
